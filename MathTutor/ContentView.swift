5ff417f614c7839d4d0a0d272f2658fc4a0769fd
Jeff Gutierrez <jeffgutierrez0524@gmail.com>
2025-10-05 14:59:51 -0400

    Initial Commit

---
+++ MathTutor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MathTutor
+//
+//  Created by Jeff Gutierrez on 10/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
